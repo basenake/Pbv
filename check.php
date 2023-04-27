99b96699cc38a7abc04c16e6fe869c18d7bb945f
wayne <carzy70203@gmail.com>
2023-04-27 14:05:29 +0800

    登入頁面

--- check.php
+++ check.php
@@ -2,18 +2,14 @@
 $acc='admin';
 $pw='1234';
 
-if ($_empty){
-    
-}
-
 $_POST=['acc'];
 $_POST=['pw'];
-
 if($_POST['acc']==$acc && $_POST['pw']==$pw){
     echo "帳密正確 歡迎登入";
 }else{
-    echo "帳密錯誤 重新輸入";
+    header("location:login.php?error=帳密錯誤 重新輸入");
 }
+
 
 ?>
 <a href="login.php">登入頁面</a>
--- login.php
+++ login.php
@@ -1,4 +1,13 @@
 <h1>登入畫面</h1>
+<!-- 老師範例 可以不用額外做另一個網頁 直接當前執行 -->
+<?php
+if(isset($_GET['error'])){
+    echo "<span  style ='color:red'>";
+    echo $_GET['error'];
+    echo "</span>";
+
+}
+?>
 <form action="check.php" method="post">
     <div>
         <label for="acc">帳號</label>
