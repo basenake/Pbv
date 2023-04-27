0bc81aa0e038b93603df16dae3966604ff04f7e7
wayne <carzy70203@gmail.com>
2023-04-27 10:46:01 +0800

    傳值

---
+++ index.php
@@ -0,0 +1,13 @@
+<h1>表單傳值</h1>
+<h3>get</h3>
+<form action="" method="get">
+    <input type="text" name="name" id="name">
+    <input type="submit" value="送出">
+</form>
+
+<h3>post</h3>
+<form action="" method="post">
+    <input type="text" name="name" id="name">
+    <input type="submit" value="送出">
+
+</form>
