f6e65c3182123e14b251b7030a7bf01c044d7869
wayne <carzy70203@gmail.com>
2023-04-27 13:34:46 +0800

    bmi 試作

---
+++ bmi.php
@@ -0,0 +1,14 @@
+<h1>bmi計算</h1>
+<form action="result.php" method="get">
+    <div>
+        <label for="hight">身高(公分)</label>
+        <input type="number" name="hight" id="hight">
+    </div>
+    <div>
+        <label for="weight">體重(公斤)</label>
+        <input type="number" name="weight" id="weight">
+    </div>
+    <div>
+        <input type="submit" value="送出">
+    </div>
+</form>
---
+++ result.php
@@ -0,0 +1,28 @@
+<?php
+$height=$_GET['height'];    //身高
+$weight=$_GET['weight'];    //體重
+
+echo "您輸入的身高為:".$height."公分 <br>";
+echo "您輸入的體重為:".$weight."公斤 <br>";
+
+$bmi=round( $weight / (($height/100)*($height/100)),2);
+// pow($height,2)
+
+$level='';
+
+if($bmi>=27){
+    $level="肥胖";
+}else if ($bmi>=24){
+    $level="體重過重";
+}else if ($bmi>=18.5){
+    $level="健康";  
+}else{
+    $level="體重過輕";
+}
+
+echo "您的BMI為:" ;
+echo "<br>";
+echo "您的體位判定為: .$level";
+echo "<br>";
+
+?>
