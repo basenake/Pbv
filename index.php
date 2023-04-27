259b88d223e8c0fcdb9509021cfea5a144f0d70b
wayne <carzy70203@gmail.com>
2023-04-27 10:48:12 +0800

    傳值介紹

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <h1>表單傳值</h1>
 <h3>get</h3>
-<form action="" method="get">
+<form action="target.php" method="get">
     <input type="text" name="name" id="name">
     <input type="submit" value="送出">
 </form>
 
 <h3>post</h3>
-<form action="" method="post">
+<form action="target.php" method="post">
     <input type="text" name="name" id="name">
+    <input type="submit" value="送出">
+</form>
+
+
+<h3>post及檔案上傳</h3>
+<form action="target.php" method="post" enctype="multipart/form-data">
+    <input type="text" name="type">
+    <input type="file" name="img" id="">
     <input type="submit" value="送出">
 
 </form>
